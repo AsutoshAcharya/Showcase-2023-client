fd60e046d519bdef2c8b0caca043b87e9f4c0b75
Bidhu1024 <bidhu1024av@gmail.com>
2023-05-11 18:16:45 +0530

    h

--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -1,8 +1,34 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
 
 function Courses() {
+  const data = [
+    {
+      id:'1',
+      name:'HTML',
+      created_by:'Dave Gray',
+      desc:'HTML stands for Hyper Text Markup Language. HTML is the standard markup language for creating Web pages. HTML describes the structure of a Web page. HTML consists of a series of elements. HTML elements tell the browser how to display the content.',
+      url:'https://youtu.be/kUMe1FH4CHE'
+    },
+    {
+      id:'2',
+      name:'CSS',
+      created_by:'Dave Gray',
+      desc:'CSS is used to define styles for your web pages, including the design, layout and variations in display for different devices and screen sizes.',
+      url:"https://youtu.be/OXGznpKZ_sA",
+    },
+    {
+      id:'2',
+      name:'CSS',
+      created_by:'Dave Gray',
+      desc:'CSS is used to define styles for your web pages, including the design, layout and variations in display for different devices and screen sizes.',
+      url:"https://youtu.be/OXGznpKZ_sA",
+    },
+  ]
   return (
-    <div>Courses</div>
+    <Box sx={{width:'100%', height:'100vh'}}>
+
+    </Box>
   )
 }
 
